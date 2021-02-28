d0734522891e74558e660b8fbcdd238785b9cbe2
kyh1126 <kyh901126@nate.com>
2021-02-28 14:26:10 +0900

    1-4. 메소드 레퍼런스

---
+++ src/main/java/me/jenny/java8to11/_1_FunctionalInterfaceAndLambda/App.java
@@ -0,0 +1,34 @@
+package me.jenny.java8to11._1_FunctionalInterfaceAndLambda;
+
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class App {
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+//        UnaryOperator<String> hiUnaryOperator = (s) -> "hi" + s;
+        // 어떤 객체의 static method 참조
+        UnaryOperator<String> hiUnaryOperator = Greeting::hi;
+
+        // 어떤 객체의 인스턴스 method 참조
+        UnaryOperator<String> hello = greeting::hello;
+
+        // 임의 객체의 인스턴스 method 참조
+        String[] names = {"a", "b", "c"};
+//        Arrays.sort(names, String::compareToIgnoreCase);
+        Arrays.sort(names, (o1, o2) -> 1);
+//        Arrays.sort(names, (o1, o2) -> -1);
+        System.out.println(Arrays.toString(names));
+
+        // 생성자 참조
+        Supplier<Greeting> newSupplier = Greeting::new;
+        Greeting newGreeting = newSupplier.get();
+
+        Function<String, Greeting> newFunction = Greeting::new;
+        Greeting newGreeting2 = newFunction.apply("jenny!");
+        System.out.println(newGreeting2.getName());
+
+    }
+}
---
+++ src/main/java/me/jenny/java8to11/_1_FunctionalInterfaceAndLambda/Greeting.java
@@ -0,0 +1,24 @@
+package me.jenny.java8to11._1_FunctionalInterfaceAndLambda;
+
+public class Greeting {
+    private String name;
+
+    public Greeting() {
+    }
+
+    public Greeting(String name) {
+        this.name = name;
+    }
+
+    public String hello(String name) {
+        return "hello " + name;
+    }
+
+    public static String hi(String name) {
+        return "hi " + name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
